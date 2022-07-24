fa348a2b96228ef0946e0bd7b26c7ab170e1b973
GuessWhoIam <13681132624@139.com>
2022-07-24 14:25:42 +0800

    修改测试用例

--- Pluto/ToyRpc/src/main/rpc/org/example/App.java
+++ Pluto/ToyRpc/src/main/rpc/org/example/App.java
@@ -1,11 +1,10 @@
-package org.example;
+package rpc.org.example;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
---
+++ Pluto/ToyRpc/src/test/rpc/org/example/AppTest.java
@@ -0,0 +1,17 @@
+//package java.org.example;
+//
+//
+///**
+// * Unit test for simple App.
+// */
+//public class AppTest
+//{
+//    /**
+//     * Rigorous Test :-)
+//     */
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+//}

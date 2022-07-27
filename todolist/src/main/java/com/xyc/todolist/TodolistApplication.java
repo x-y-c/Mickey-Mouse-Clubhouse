d8aa72f35dc7ef39fd787b8d02f02a6aba63275f
anotheryy <13681132624@139.com>
2022-07-28 00:00:41 +0800

    data

--- todolist/src/main/java/com/xyc/todolist/TodolistApplication.java
+++ todolist/src/main/java/com/xyc/todolist/TodolistApplication.java
@@ -8,7 +8,7 @@ public class TodolistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
-		//todo
+		//todo database init profile
 	}
 
 }

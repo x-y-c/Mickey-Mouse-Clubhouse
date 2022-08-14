f462e98293b70616b1d3878c4d9dc55acf5860bf
yangchen <13681132624@163.com>
2022-08-14 17:03:38 +0800

    登录验证拦截器

---
+++ todolist/src/main/java/com/xyc/todolist/common/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package com.xyc.todolist.common;
+
+import com.xyc.todolist.anno.Login;
+import com.xyc.todolist.utils.JWTUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("token");
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Method method = handlerMethod.getMethod();
+        if (method.isAnnotationPresent(Login.class)) {
+            Login login = method.getAnnotation(Login.class);
+            if (login.required()) {
+                if (token == null) {
+                    throw new RuntimeException("token信息无效，请登录");
+                }
+                String s = JWTUtils.validateToken(token);
+                return s != null;
+            }
+        }
+
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
---
+++ todolist/src/main/java/com/xyc/todolist/common/WebConfig.java
@@ -0,0 +1,20 @@
+package com.xyc.todolist.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor()).addPathPatterns("/**");
+    }
+
+    @Bean
+    public LoginInterceptor loginInterceptor(){
+        return new LoginInterceptor();
+    }
+}

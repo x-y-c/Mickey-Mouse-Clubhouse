4ef7f1a29dd89d2a18fe6d28ec71a95d0d39cd9f
yangchen <13681132624@163.com>
2022-08-21 19:58:55 +0800

    成员组dto修改

--- todolist/src/main/java/com/xyc/todolist/dto/TeamUserDto.java
+++ todolist/src/main/java/com/xyc/todolist/dto/TeamUserDto.java
@@ -10,12 +10,17 @@ public class TeamUserDto {
     private Integer teamId;
 
     /**
-     *  管理员用户组（表示添加的管理员）
+     * 管理员用户组（表示添加的管理员）
      */
-    private List<Integer>adminUsers;
+    private List<Integer> adminUsers;
 
     /**
      * 标准用户组
      */
-    private List<Integer>normalUsers;
+    private List<Integer> normalUsers;
+
+    /**
+     * 需要删除的用户
+     */
+    private List<Integer> deleteUsers;
 }

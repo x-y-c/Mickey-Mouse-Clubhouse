52ef55ef030844ebd4dbf314fa6a50f8a660e806
yangchen <13681132624@163.com>
2022-09-04 16:33:43 +0800

    更新todoService

--- todolist/src/main/java/com/xyc/todolist/service/TodoListService.java
+++ todolist/src/main/java/com/xyc/todolist/service/TodoListService.java
@@ -1,6 +1,9 @@
 package com.xyc.todolist.service;
 
+import com.mysql.cj.util.EscapeTokenizer;
 import com.xyc.todolist.entity.TodoList;
+
+import java.util.List;
 
 public interface TodoListService {
 
@@ -25,4 +28,19 @@ public interface TodoListService {
      * @return
      */
     public TodoList updateTodoInfo(TodoList todoList);
+
+    /**
+     * 获取todo清单
+     * @param userId
+     * @return
+     */
+    public List<TodoList>getTodolist(Integer userId);
+
+    /**
+     * 获取不同类型的todo清单，如 已完成 未完成 等等
+     * @param userId
+     * @param type
+     * @return
+     */
+    public List<TodoList> getTodoList(Integer userId,Integer type);
 }

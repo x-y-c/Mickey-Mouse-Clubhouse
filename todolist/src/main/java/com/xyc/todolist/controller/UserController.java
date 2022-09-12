9b51016b5aeeb46866f0367fe8e36941c96d634e
yangchen <13681132624@163.com>
2022-09-12 21:31:34 +0800

    usercontroller

--- todolist/src/main/java/com/xyc/todolist/controller/UserController.java
+++ todolist/src/main/java/com/xyc/todolist/controller/UserController.java
@@ -16,15 +16,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
     public JsonResult register(@RequestBody User user) {
         JsonResult register = userService.register(user);
         return register;
     }
 
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public JsonResult login(String username,String password){
-       return userService.login(username,password);
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public JsonResult login(String username, String password) {
+        return userService.login(username, password);
+    }
+
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public JsonResult updateUserInfo(User user) {
+        //这个修改密码的逻辑有bug，一般是先填写邮箱，然后邮箱中接收到邮件，在邮件中包含重置链接，最后通过链接重置密码
+        JsonResult jsonResult = userService.resetPassword(user.getUsername(), user.getEmail(), user.getPassword());
+        return jsonResult;
     }
 
 }

176f0ee4fcfdc2a8ee89ca936d8699b2cc1e5057
yangchen <13681132624@163.com>
2022-10-16 22:03:21 +0800

    update todoController

--- todolist/src/main/java/com/xyc/todolist/TodolistApplication.java
+++ todolist/src/main/java/com/xyc/todolist/TodolistApplication.java
@@ -8,7 +8,6 @@ public class TodolistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
-		//todo database init profile
 	}
 
 }
---
+++ todolist/src/main/java/com/xyc/todolist/controller/TodoController.java
@@ -0,0 +1,30 @@
+package com.xyc.todolist.controller;
+
+import com.sun.tools.javac.comp.Todo;
+import com.xyc.todolist.entity.TodoList;
+import com.xyc.todolist.service.TodoListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/todo")
+public class TodoController {
+
+    @Autowired
+    private TodoListService todoListService;
+
+    //创建todo
+    @PostMapping("/create")
+    public TodoList createTodo(@RequestBody TodoList todoList) {
+        //validUser 自己创建或者同组间创建
+        TodoList todo = todoListService.createTodo(todoList);
+        return todo;
+    }
+
+    //编辑todo
+
+
+    //删除todo
+}

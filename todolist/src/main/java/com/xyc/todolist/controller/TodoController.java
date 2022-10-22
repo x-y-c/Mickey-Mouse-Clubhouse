3084a1ffd7ef2f8af8f392e49b79e0f242a6b2fd
yangchen <13681132624@163.com>
2022-10-22 20:38:49 +0800

    update controller

--- todolist/src/main/java/com/xyc/todolist/controller/TodoController.java
+++ todolist/src/main/java/com/xyc/todolist/controller/TodoController.java
@@ -1,14 +1,19 @@
 package com.xyc.todolist.controller;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.sun.tools.javac.comp.Todo;
 import com.xyc.todolist.anno.Login;
 import com.xyc.todolist.entity.TodoList;
 import com.xyc.todolist.service.TodoListService;
+import com.xyc.todolist.utils.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController("/todo")
 public class TodoController {
@@ -26,6 +31,14 @@ public class TodoController {
     }
 
     //编辑todo
+    @Login
+    @PostMapping
+    public TodoList updateTodo(HttpServletRequest request, @RequestBody TodoList todoList) {
+        String token = request.getHeader("token");
+        DecodedJWT tokenInfo = JWTUtils.getTokenInfo(token);
+        Integer userId = JWTUtils.getUserId(tokenInfo);
+        return todoListService.updateTodoInfo(todoList, userId);
+    }
 
 
     //删除todo

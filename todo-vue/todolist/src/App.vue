a4f71cb308dbe05330d709c0db515a0c29a55c24
anotheryy <13681132624@139.com>
2022-11-13 22:47:45 +0800

    cleanup

--- todo-vue/todolist/src/App.vue
+++ todo-vue/todolist/src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import Index from "../index.html";
 </script>
 
 <template>
@@ -17,17 +16,4 @@ import Index from "../index.html";
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>

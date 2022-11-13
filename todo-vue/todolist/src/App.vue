c7216df0878be3079ceaf1902d72f031bfca6e53
anotheryy <13681132624@139.com>
2022-11-13 23:06:56 +0800

    todo view

--- todo-vue/todolist/src/App.vue
+++ todo-vue/todolist/src/App.vue
@@ -10,6 +10,11 @@
         <input type="text" class="input-todo" v-bind:class="{active: new_todo }" placeholder="qingchu" value="new _todo" v-on:keyup.enter="addItem">
         <div class="btn btn-add" v-bind:class="{ active: new_todo}" @click="addItem">+</div>
       </form>
+      <div v-if="pending.length > 0">
+        <p class="status busy">you have {{ pending.length }} pending item <span v-if="pending.length>1">s</span>  </p>
+//todo <transition-group></transition-group>
+
+      </div>
 
     </div>
   </div>

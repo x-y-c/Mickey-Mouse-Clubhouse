7486a1f41e21949a403afcedded591fde7e305d7
anotheryy <13681132624@139.com>
2022-11-13 22:41:25 +0800

    html

--- todo-vue/todolist/src/App.vue
+++ todo-vue/todolist/src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
-import Drag from "./components/drag.vue"
+import Index from "../index.html";
 </script>
 
 <template>
   <div>
     <h1>TODO list</h1>
+    <div class="todo-wrapper">
+      <h2 class="todo-title">{{today.day}}<br>{{today.date}}</h2>
+      <form @keydown.enter.prevent="">
+        <input type="text" class="input-todo" v-bind:class="{active: new_todo }" placeholder="qingchu" value="new _todo" v-on:keyup.enter="addItem">
+        <div class="btn btn-add" v-bind:class="{ active: new_todo}" @click="addItem">+</div>
+      </form>
 
+    </div>
   </div>
 </template>
 
@@ -15,9 +22,11 @@ import Drag from "./components/drag.vue"
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }

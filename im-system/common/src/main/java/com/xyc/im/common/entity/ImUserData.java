dd0dd78171121090fb16b261ed9dcc7ce9b9cba8
anotheryy <13681132624@139.com>
2023-10-09 23:23:41 +0800

    修改项目结构2

---
+++ im-system/common/src/main/java/com/xyc/im/common/entity/ImUserData.java
@@ -0,0 +1,25 @@
+package com.xyc.im.common.entity;
+
+
+import java.util.Date;
+
+public class ImUserData {
+    private  Integer id;
+    private String userId;
+    private Integer appId;
+    private String nickName;
+    private Integer userSex;
+    private Date birthDay;
+    private String location;
+    private String selfSignature;
+    private Integer friendAllowType;
+    private String photo;
+    private String password;
+    private Integer disableAddFriend;
+    private Integer silentFlag;
+    private Integer forbiddenFlag;
+    private Integer userType;
+    private Integer delFlag;
+    private String extra;
+
+}

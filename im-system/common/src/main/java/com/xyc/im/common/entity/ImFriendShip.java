2d898cd263acf309e379e86171a5e18f50cea2af
anotheryy <13681132624@139.com>
2023-10-14 17:25:47 +0800

    修改/添加好友关系表

--- im-system/common/src/main/java/com/xyc/im/common/entity/ImFriendShip.java
+++ im-system/common/src/main/java/com/xyc/im/common/entity/ImFriendShip.java
@@ -1,5 +1,22 @@
-package common.src.main.java.com.xyc.im.common.entity;
+package src.main.java.com.xyc.im.common.entity;
+
+import java.util.Date;
+
 
 public class ImFriendShip{
+    private Integer appId;
+    private String fromId;
+    private String toId;
+    private String remark;
+    private Integer status;
+    private Integer black;
+    private Date createTime;
+    private Integer friendSequence;
+
+    private Integer blackSequence;
+    private String addSource;
+    private String extra;
+
+
 
 }

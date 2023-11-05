174e5dd34bee575bd219f7e26286f72578235f2d
anotheryy <13681132624@139.com>
2023-11-05 23:33:19 +0800

    im message

---
+++ im-system/service/src/main/java/com/xyc/im/service/message/ImMessageBody.java
@@ -0,0 +1,17 @@
+package com.xyc.im.service.message;
+
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "im_message_body")
+public class ImMessageBody {
+    private Integer appId;
+    private Long messageKey;
+    private String messageBody;
+    private String securityKey;
+    private Long messageTime;
+    private Long createTime;
+    private String extra;
+    private Integer delFlag;
+}

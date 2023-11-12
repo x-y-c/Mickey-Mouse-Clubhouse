d30b8d9483e6fe4a212f01d2db083f2137873ecd
yangchen <13681132624@163.com>
2023-11-12 12:32:54 +0800

    群消息

---
+++ im-system/service/src/main/java/com/xyc/im/service/group/ImGroupMessageHistory.java
@@ -0,0 +1,22 @@
+package com.xyc.im.service.group;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "im_group_message_history")
+public class ImGroupMessageHistory {
+    @Id
+    private Long id;
+    private Integer appId;
+    private String fromId;
+    private String groupId;
+    private Long messageKey;
+
+    private Long sequence;
+    private String messageRandom;
+
+    private Long messageTime;
+    private Long createTime;
+}

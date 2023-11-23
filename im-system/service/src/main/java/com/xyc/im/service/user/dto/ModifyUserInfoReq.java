b38b7e0b51738df6896371dee190b39a25b3f0e4
yangchen <13681132624@163.com>
2023-11-23 22:35:29 +0800

    friendreq

---
+++ im-system/service/src/main/java/com/xyc/im/service/user/dto/ModifyUserInfoReq.java
@@ -0,0 +1,19 @@
+package com.xyc.im.service.user.dto;
+
+public class ModifyUserInfoReq {
+    private String userId;
+    private String location;
+    private String birthday;
+    private String password;
+
+    private String phone;
+    private String userSex;
+    private String selfSignature;
+
+    /**
+     * 加好友验证
+     */
+    private Integer friendAllowType;
+
+    private String extra;
+}

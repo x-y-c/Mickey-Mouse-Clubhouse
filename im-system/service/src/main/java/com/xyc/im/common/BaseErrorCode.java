a428ff935072b43f21b6e036b91386428e80bd3e
yangchen <13681132624@163.com>
2023-12-03 22:26:46 +0800

    errorcode

---
+++ im-system/service/src/main/java/com/xyc/im/common/BaseErrorCode.java
@@ -0,0 +1,27 @@
+package com.xyc.im.common;
+
+import com.xyc.im.common.exception.ApplicationExceptionEnum;
+
+public enum BaseErrorCode implements ApplicationExceptionEnum {
+    SUCCESS(200,"success"),
+    SYSTEM_ERROR(90000,"fu服务器内部错误,请联系管理员"),
+    PARAMETER_ERROR(90001,"参数校验错误");
+    ;
+    private int code;
+    private String error;
+
+    BaseErrorCode(int code, String error){
+        this.code =code;
+        this.error =error;
+    }
+
+    @Override
+    public int getCode() {
+        return 0;
+    }
+
+    @Override
+    public String getError() {
+        return null;
+    }
+}

80a191e2f81e42b8268e382cd57138b212a00883
anotheryy <13681132624@139.com>
2023-12-03 23:50:30 +0800

    common exception

--- im-system/service/src/main/java/com/xyc/im/common/BaseErrorCode.java
+++ im-system/service/src/main/java/com/xyc/im/common/BaseErrorCode.java
@@ -6,7 +6,6 @@ public enum BaseErrorCode implements ApplicationExceptionEnum {
     SUCCESS(200,"success"),
     SYSTEM_ERROR(90000,"服务器内部错误,请联系管理员"),
     PARAMETER_ERROR(90001,"参数校验错误");
-    ;
     private int code;
     private String error;
 

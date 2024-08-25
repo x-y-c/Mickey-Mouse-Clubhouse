b5ebf434fa7828066bb5befae702a3945396218b
yangchen <13681132624@163.com>
2024-08-25 16:57:42 +0800

    添加基础配置文件-添加注释

--- im-system/service/src/main/java/com/xyc/im/codec/config/BootstrapConfig.java
+++ im-system/service/src/main/java/com/xyc/im/codec/config/BootstrapConfig.java
@@ -6,13 +6,33 @@ import lombok.Data;
 public class BootstrapConfig {
 
     public static class TcpConfig {
+        /**
+         * tcp需要绑定的端口号
+         */
         private Integer tcpPort;
+        /**
+         * webSocket绑定的端口号
+         */
         private Integer webSocketPort;
+        /**
+         * 是否开启webSocket
+         */
         private boolean enableWebSocket;
+        /**
+         * boss 线程
+         */
         private Integer bossThreadSize;
+        /**
+         * 工作线程netty
+         */
         private Integer workThreadSize;
-
+        /**
+         * 心跳检测时间，ms
+         */
         private Long heartBeatTime;
+        /**
+         * 登录模式
+         */
         private Integer loginModel;
 
     }

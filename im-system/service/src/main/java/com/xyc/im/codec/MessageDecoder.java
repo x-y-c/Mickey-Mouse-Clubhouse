f785d7beeb8fee939898c0fd531602ea28f24443
yangchen <13681132624@163.com>
2024-08-31 20:57:45 +0800

    添加依赖，修改decode

--- im-system/service/src/main/java/com/xyc/im/codec/MessageDecoder.java
+++ im-system/service/src/main/java/com/xyc/im/codec/MessageDecoder.java
@@ -1,6 +1,12 @@
 package com.xyc.im.codec;
 
-public class MessageDecoder extends ByteToMessageDecoder{
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class MessageDecoder extends ByteToMessageDecoder {
     @Override
     protected void decode(ChannelHandlerContext ctx,
                           ByteBuf in, List<Object> out) throws Exception {
@@ -23,4 +29,6 @@ public class MessageDecoder extends ByteToMessageDecoder{
 
         out.add(message);
     }
+
+
 }

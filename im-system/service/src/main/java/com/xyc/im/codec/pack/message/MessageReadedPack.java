720158e5966be0ce3b4e29d629e40ecb426ca6f2
yangchen <13681132624@163.com>
2024-09-14 22:16:52 +0800

    massage about

---
+++ im-system/service/src/main/java/com/xyc/im/codec/pack/message/MessageReadedPack.java
@@ -0,0 +1,16 @@
+package com.xyc.im.codec.pack.message;
+
+import lombok.Data;
+
+@Data
+public class MessageReadedPack {
+    private long messageSequence;
+
+    private String fromId;
+
+    private String groupId;
+
+    private String toId;
+
+    private Integer conversationType;
+}

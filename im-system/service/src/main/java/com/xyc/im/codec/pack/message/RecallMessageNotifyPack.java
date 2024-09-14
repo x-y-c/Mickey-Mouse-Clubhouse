af889f7d317d6a5c8682f72f9350dee2663642a9
anotheryy <13681132624@139.com>
2024-09-14 22:23:41 +0800

    commit about message detail

--- im-system/service/src/main/java/com/xyc/im/codec/pack/message/RecallMessageNotifyPack.java
+++ im-system/service/src/main/java/com/xyc/im/codec/pack/message/RecallMessageNotifyPack.java
@@ -1,4 +1,17 @@
 package com.xyc.im.codec.pack.message;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RecallMessageNotifyPack {
+
+    private String fromId;
+
+    private String toId;
+
+    private Long messageKey;
+
+    private Long messageSequence;
 }

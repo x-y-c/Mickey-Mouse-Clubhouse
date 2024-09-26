6615354db5ffa463eb9295e194613e03d7fc8ff8
yangchen <13681132624@163.com>
2024-09-26 11:25:55 +0800

    massage about

--- im-system/service/src/main/java/com/xyc/im/codec/pack/group/GroupMessagePack.java
+++ im-system/service/src/main/java/com/xyc/im/codec/pack/group/GroupMessagePack.java
@@ -1,7 +1,11 @@
 package com.xyc.im.codec.pack.group;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class GroupMessagePack {
     private String messageId;

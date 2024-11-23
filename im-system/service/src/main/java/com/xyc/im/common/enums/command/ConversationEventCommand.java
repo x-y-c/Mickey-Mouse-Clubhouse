a513862d50608c7ba5f77952763a1e7c915b74b2
yangchen <13681132624@163.com>
2024-11-23 20:18:31 +0800

    会话事件

---
+++ im-system/service/src/main/java/com/xyc/im/common/enums/command/ConversationEventCommand.java
@@ -0,0 +1,25 @@
+package com.xyc.im.common.enums.command;
+
+public enum ConversationEventCommand implements Command {
+
+    //删除会话
+    CONVERSATION_DELETE(5000),
+
+    //删除会话
+    CONVERSATION_UPDATE(5001),
+
+    ;
+
+    private int command;
+
+    ConversationEventCommand(int command){
+        this.command=command;
+    }
+
+
+    @Override
+    public int getCommand() {
+        return command;
+    }
+}
+
